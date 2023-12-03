8f6c916d6ad740fc454e3b1feb568a79ebb9a746
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2023-12-03 19:07:56 +0545

    dapper context

--- DapperAspNetCore/Models/Company.cs
+++ DapperAspNetCore/Models/Company.cs
@@ -6,6 +6,6 @@
         public string? Name { get; set; }    
         public string? Address { get; set; }
         public string? Country { get; set; } 
-        public List<Employee> Employees { get; set; }   = new List<Employee>();
+        public List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
